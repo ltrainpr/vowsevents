260fb8431aa2d981948eb490c2b7459287b13776
Lionel E. Ramos <ltrainpr@gmail.com>
2014-02-13 10:21:06 -0700

    Feature spec to make image upload form

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,9 @@
 class Image < ActiveRecord::Base
 	belongs_to :event
 	belongs_to :testimonial
+
+	has_attached_file :s3_image_url, styles: {
+		medium: '300x300>'
+	}
+	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
---
+++ spec/features/upload_photo_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+feature 'Visit upload_photo page' do
+	scenario 'sees form to upload photo' do
+		visit upload_photo_path
+
+		expect(page).to have_title 'Upload Photo'
+		expect(page).to have_css '.field'
+	end
+end
