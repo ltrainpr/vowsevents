496be265489cba466e890b72d7246e18c09c442c
Lionel E. Ramos <ltrainpr@gmail.com>
2014-02-26 15:27:53 -0700

    Centers gallery images and refactors JavaScript

--- app/assets/javascripts/navigation-bar.js
+++ app/assets/javascripts/navigation-bar.js
@@ -2,8 +2,6 @@
 	$("#nav-menu").on('click', showMenu);
 
 	function showMenu(e) {
-		e.stopImmediatePropagation();
-		console.log('clicking');
 		$('#navigation').toggleClass('navigation-menu');
 		$('.nav-item').toggleClass('hide');
 		$('.menu-services').toggleClass('pad-right');
