f2f885bfe5d13c8876ed214d286e455cf1ffde06
Lionel E. Ramos <ltrainpr@gmail.com>
2014-02-27 10:31:18 -0700

    Links images to event selection on gallery

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,7 +1,9 @@
 class GalleryController < ApplicationController
 	def index
-		last_event = Event.first
-		photos = last_event.images.last(3)
+		@events = Event.all
+		params[:event_id] ? event_found = Event.find(params[:event_id]) : event_found = nil
+		event = event_found || Event.first
+		photos = event.images.last(3)
 		@photo1 = photos[0]
 		@photo2 = photos[1]
 		@photo3 = photos[2]
