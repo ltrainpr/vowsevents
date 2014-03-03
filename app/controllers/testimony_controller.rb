7f372f44fec85ff346d4699f790bb16d36fb5e12
Lionel E. Ramos <ltrainpr@gmail.com>
2014-03-03 16:43:12 -0700

    Testimonial page

--- app/controllers/testimony_controller.rb
+++ app/controllers/testimony_controller.rb
@@ -1,9 +1,12 @@
 class TestimonyController < ApplicationController
   def index
-  	
+  	@jen_rob = Testimonial.find(1)
+  	@rhonda_maynard = Testimonial.find(2)
+  	@jen_parents = Testimonial.find(3)
   end
 
   def show
+  	@testimonial = Testimonial.find(params[:id])
   	
   end
 end
