87e5efbda3653f3c0d66659ee48fce29c27b2997
Lionel E. Ramos <ltrainpr@gmail.com>
2014-03-03 18:05:51 -0700

    BUGFIX: Testimonial needs to be larger.  Removes iFrame

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
 	belongs_to :event
 	belongs_to :testimonial
 
-	has_attached_file :s3_image_url, styles: {medium: '600x390>', large: '1000x600>'}, :storage => :s3, :url => ":s3_domain_url"
+	has_attached_file :s3_image_url, styles: {medium: '600x390>', large: '926x1196>'}, :storage => :s3, :url => ":s3_domain_url"
 	validates_attachment_content_type :s3_image_url, :content_type => /\Aimage\/.*\Z/
 	validates_attachment_presence :s3_image_url
 end
