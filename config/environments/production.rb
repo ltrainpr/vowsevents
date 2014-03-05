5a42f3a4fef6c7dad7defc6588ede9d067ccfd34
Lionel E. Ramos <ltrainpr@gmail.com>
2014-03-05 12:09:07 -0700

    Updates configuration environment on production for Action Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,8 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
@@ -84,6 +85,17 @@ Rails.application.configure do
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
-}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'vowsevents.com',
+    user_name:            ENV['EMAIL_LOGIN'],
+    password:             ENV['EMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 end
