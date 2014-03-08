9307cf77e4df2ea5a3b17710451c4663d177ad10
Lionel E. Ramos <ltrainpr@gmail.com>
2014-03-07 22:18:40 -0700

    Saved files from Eric's Dropbox. Cleaned and updated the contact.html form

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -3,9 +3,15 @@ class GalleryController < ApplicationController
 		@events = Event.all
 		params[:event_id] ? event_found = Event.find(params[:event_id]) : event_found = nil
 		event = event_found || Event.first
-		photos = event.images.where(testimonial_id: nil).last(3)
+		photos = event.images.where(testimonial_id: nil).last(8)
 		@photo1 = photos[0]
 		@photo2 = photos[1]
 		@photo3 = photos[2]
+		@photo4 = photos[3]
+		@photo5 = photos[4]
+		@photo6 = photos[5]
+		@photo7 = photos[6]
+		@photo8 = photos[7]
+		@photo_count = photos.count
 	end
 end
