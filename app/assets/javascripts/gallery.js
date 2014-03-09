f5c59d310508659c3eccc50fa0b08b6a5642d9bb
Lionel E. Ramos <ltrainpr@gmail.com>
2014-03-08 17:51:14 -0700

    Trying gallery with long div and position absolute

---
+++ app/assets/javascripts/gallery.js
@@ -0,0 +1,26 @@
+
+// $(window).load(function centerImages() {
+// 	    var photos = $('.photos');
+// 	    var sum = 0;
+// 	    var photoLeftOffset = 0;
+// 	    $.each(photos, function (index, photo) {
+// 	    	$(photo).css("left", sum) 
+// 	    	sum += $(photo).width()
+// 	    });
+// 	    $('.long-div').width(sum)
+// 	    // find what lies beyond the halfway point of the 1000px container div
+// 	    // by subtracting half of it from half of the center image and all of the left image
+// 	    		leftPosition =  leftImage - (500 - (centerImage/2));
+	    
+// 	    $('.slide-left').on('click', function(){
+// 	        $('.long-div').animate({
+// 		    		left: '-'+ leftPosition + 'px'
+// 	        }, 500);
+// 	    	}
+//     	});
+
+// 	    $('.slide-right').on('click', function(){
+//         $('.long-div').animate({
+// 	    		left: '-'+leftPosition+'px'
+//         }, 500);
+// 	    });
