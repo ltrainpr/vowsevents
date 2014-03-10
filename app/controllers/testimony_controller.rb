b075241809e39fba66d364a6af13f11b46f57ddd
Lionel E. Ramos <ltrainpr@gmail.com>
2014-03-10 10:54:46 -0600

    Updates Testimonial page to show all testimonials

--- app/controllers/testimony_controller.rb
+++ app/controllers/testimony_controller.rb
@@ -1,13 +1,9 @@
 class TestimonyController < ApplicationController
   def index
-  	@jen_rob = Testimonial.find(1)
-  	@rhonda_maynard = Testimonial.find(2)
-  	@jen_parents = Testimonial.find(3)
+    @testimonials = Testimonial.all
   end
 
   def show
-  	p Testimonial.where(event_id: params[:event_id]).first
   	@testimonial = Testimonial.where(event_id: params[:event_id]).first
-  	
   end
 end
--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,4 +1,5 @@
 class Testimonial < ActiveRecord::Base
 	has_many :images
 	belongs_to :event
+	validates :quote, :image_id, :event_id, presence: true
 end
