932494ce45c3af6b023ec091de150981e3cf0dc7
Lionel E. Ramos <ltrainpr@gmail.com>
2014-03-10 10:19:25 -0600

    Moves JavaScript in Other Services page to new other_services.js file

---
+++ app/assets/javascripts/other_services.js
@@ -0,0 +1,30 @@
+
+function showSS() {
+	document.getElementById("services-description-default").className = "hide";
+	document.getElementById("services-description-ss").className = "show";
+	document.getElementById("services-description-ss").className += " pad-top"; }
+function showWG() {
+	document.getElementById("services-description-default").className = "hide";
+	document.getElementById("services-description-wg").className = "show";
+	document.getElementById("services-description-wg").className += " pad-top"; }
+function showBT() {
+	document.getElementById("services-description-default").className = "hide";
+	document.getElementById("services-description-bt").className = "show";
+	document.getElementById("services-description-bt").className += " pad-top"; }
+function showDI() {
+	document.getElementById("services-description-default").className = "hide";
+	document.getElementById("services-description-di").className = "show";
+	document.getElementById("services-description-di").className += " pad-top"; }
+function hideSS() {
+	document.getElementById("services-description-ss").className = "hide";
+	document.getElementById("services-description-default").className = "show";}
+function hideWG() {
+	document.getElementById("services-description-wg").className = "hide";
+	document.getElementById("services-description-default").className = "show";	}
+function hideBT() {
+	document.getElementById("services-description-bt").className = "hide";
+	document.getElementById("services-description-default").className = "show";}
+function hideDI() {
+	document.getElementById("services-description-di").className = "hide";
+	document.getElementById("services-description-default").className = "show";	}
+
