03c83b214b0db23806639dd5b3f5484d82666c82
Lionel E. Ramos <ltrainpr@gmail.com>
2014-03-14 20:24:09 -0600

    REFACTOR: Cleaned up JS in for about_us page

--- app/assets/javascripts/about_us.js
+++ app/assets/javascripts/about_us.js
@@ -1,35 +1,13 @@
-var toggleHide = function (){
-	return $('#a-thumb, #k-thumb, #s-thumb, #directionz, #group_photo').toggleClass('hide');
-};
-function showKA() {
-	toggleHide();
-	document.getElementById("about-karin").className = "show";
-	document.getElementById("group-thumb").className = "show";
-	document.getElementById("about-karin").className += " about_indi";}
 
-function showAV() {
-	toggleHide();
-	document.getElementById("about-avery").className = "show";
-	document.getElementById("group-thumb").className = "show";
-	document.getElementById("about-avery").className += " about_indi";}
+$('.about_photo').on('click', function (e){
+	var indie = $(e.currentTarget).data('indie');
+	$('.toggle-hide').toggleClass('hide');
+	$('#about-' + indie).removeClass('hide');
+});
 
-function showSI() {
-	toggleHide();
-	document.getElementById("about-simona").className = "show";
-	document.getElementById("group-thumb").className = "show";
-	document.getElementById("about-simona").className += " about_indi";}
+$('#group-thumb').on ('click', showGroup);
 
 function showGroup() {
-	document.getElementById("about-avery").className = "hide";
-	document.getElementById("about-simona").className = "hide";
-	document.getElementById("about-karin").className = "hide";
-	document.getElementById("group-thumb").className = "hide";
-	document.getElementById("group_photo").className = "show";
-	document.getElementById("a-thumb").className = "show";
-	document.getElementById("a-thumb").className += " avery about_photo";
-	document.getElementById("k-thumb").className = "show";
-	document.getElementById("k-thumb").className += " karin about_photo";
-	document.getElementById("s-thumb").className = "show";
-	document.getElementById("s-thumb").className += " simona about_photo";
-	document.getElementById("directionz").className = "show";
+	$('.show-group').addClass('hide');
+	$('.toggle-hide').toggleClass('hide');
 }
