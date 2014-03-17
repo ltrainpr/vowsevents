310fae5493deb9fbfeadf1c91ace91b60ed557e2
Lionel E. Ramos <ltrainpr@gmail.com>
2014-03-17 16:37:03 -0600

    Factory already registered: event (FactoryGirl::DuplicateDefinitionError)

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,23 +4,23 @@ describe Event do
   context "model validates presence of name & date" do
   	it "should be valid" do
   		
-  		expect(Factory.build(:event)).to be_valid	
+  		expect(build(:event)).to be_valid	
   	end
 
   	it "should not be valid" do
   	
-  		expect(Factory.build(:event, name: "")).not_to be_valid
-  		expect(Factory.build(:event, date: nil)).not_to be_valid
+  		expect(build(:event, name: "")).not_to be_valid
+  		expect(build(:event, date: nil)).not_to be_valid
   	end
   end
   context "model validates format of date" do
   	it "should be valid" do
 
-  		expect(Factory.build(:event, date: Date.new(2014, 3, 17))).to be_valid
+  		expect(build(:event, date: Date.new(2014, 3, 17))).to be_valid
   	end
 
   	it "should not be valid" do
-  		expect(Factory.build(:event, date: Date.new(14, 3, 17))).not_to be_valid
+  		expect(build(:event, date: Date.new(14, 3, 17))).not_to be_valid
   	end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rspec'
-require 'factory_girl_rails'
+
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -29,5 +29,6 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  config.include FactoryGirl::Syntax::Methods
 
 end
