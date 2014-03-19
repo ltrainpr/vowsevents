988cc8c3abdc29a2e4f33159db41d8f197baf9bf
Lionel E. Ramos <ltrainpr@gmail.com>
2014-03-19 13:29:17 -0600

    Adds event model spec that checks if record is being saved

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe Event do
   context "model validates presence of name & date" do
-  	let(:event) {build(:event)}
     
     it "should be valid" do
   		event = build(:event)
+
   		expect(event).to be_valid	
   	end
 
@@ -14,5 +14,13 @@ describe Event do
   		expect(build(:event, name: "")).not_to be_valid
   		expect(build(:event, date: nil)).not_to be_valid
   	end
+  end
+
+  context "model saves record" do
+    it "should save record" do
+      event = create(:event)
+
+      expect(Event.last).to eq event
+    end
   end
 end
