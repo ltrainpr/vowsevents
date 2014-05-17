f720db19c8559944c038843ac6d7325ed31fb96d
Lionel E. Ramos <ltrainpr@gmail.com>
2014-05-17 09:35:19 -0600

    BUGFIX: undefined method url for Testimonial object

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,5 +1,5 @@
 class Vendor < ActiveRecord::Base
 	has_many :events_vendors
 	has_many :events, :through => :event_vendors
-	validates :url, format: {with: /\Ahttp:\/\//, message: "need to add http://"}, allow_blank: true
+	validates :url, format: {with: /\Ahttp(|s):\/\//i, message: "Url needs to start with http:// or https://"}, allow_blank: true
 end
