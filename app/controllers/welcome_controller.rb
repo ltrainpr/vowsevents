506aaeafc34885e49814feedc643aa5fd895dd08
Lionel E. Ramos <ltrainpr@gmail.com>
2014-05-17 09:09:25 -0600

    BUGFIX: NoMethodError (undefined method testimonials for nil:NilClass)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,9 @@ require 'json'
 class WelcomeController < ApplicationController
 	def index
 		@event = Event.last
-		@event.testimonials.first ? @latest_testimonial = @event.testimonials.first.id : @latest_testimonial = nil
+    if @event
+		  @event.testimonials.first ? @latest_testimonial = @event.testimonials.first.id : @latest_testimonial = nil
+    end
     res = Net::HTTP.get_response(URI("http://api.tumblr.com/v2/blog/vowsevents.tumblr.com/posts/text?api_key=#{ENV['TUMBLR_API_KEY']}&limit=1"))
     parsed_json = JSON.parse(res.body)
     JSON.pretty_generate(parsed_json['response']['posts'])
@@ -12,6 +14,5 @@ class WelcomeController < ApplicationController
     parsed_date = DateTime.parse(date)
     @date = parsed_date.strftime("%b %d %Y")
     @title = parsed_json['response']['posts'][0]['title']
-    # @body = parsed_json['response']['posts'][0]['body']
 	end
 end
